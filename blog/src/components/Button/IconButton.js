4805b305ac4bb649eb5d6983b3dc4f540a1101a2
Cody Christian <codychristian@Codys-Air.attlocal.net>
2023-04-16 20:58:39 -0500

    Completed Activity 8

--- blog/src/components/Button/IconButton.js
+++ blog/src/components/Button/IconButton.js
@@ -4,18 +4,20 @@ import styled from 'styled-components'
 import { BaseButton } from './BaseButton'
 
 const StyledButton = styled(BaseButton)`
-//theme styles go here later
+    ${ ( { theme, variant }) => theme.variants.iconButton[variant || 'primary']}
 `
 
 export const IconButton = styled(({ icon, ...rest }) => {
-    let clone = React.cloneElement(icon,rest)
+    let clone = React.cloneElement( icon, rest )
     return <StyledButton as={clone.type} {...rest} className={rest.className} />
 })
+``
 
 IconButton.defaultProps = {
     size: 24
 }
 
 IconButton.propTypes = {
-    icon: PropTypes.node.isRequired
+    icon: PropTypes.node.isRequired,
+    variant: PropTypes.string
 }
